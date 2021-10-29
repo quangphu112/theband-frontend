830a70d1ac1a98d3404078964f831cde2eb2c216
Quang Phú <quangphu112@gmail.com>
2021-10-29 20:07:27 +0700

    first commit

---
+++ acsetes/js/main.js
@@ -0,0 +1,32 @@
+
+
+var buyBtns = document.querySelectorAll('.js-buy-ticket');
+
+for(var buyBtn of buyBtns) {
+    buyBtn.addEventListener('click', function() {
+        document.querySelector('.js-modal').classList.add('modal-open');
+    });
+}
+
+
+
+// $(document).ready(function() {
+//     $('.js-modal-close').click(function() {
+//         $('.js-modal').removeClass('modal-open')
+//     })
+// });
+
+document.querySelector('.js-modal-close').addEventListener('click', function() {
+    document.querySelector('.js-modal').classList.remove('modal-open');
+});
+
+document.querySelector('.js-modal').addEventListener('click', function() {
+    document.querySelector('.js-modal').classList.remove('modal-open');
+});
+
+
+document.querySelector('.js-modal-container').addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+
+
